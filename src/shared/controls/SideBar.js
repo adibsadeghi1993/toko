7d12bc5634de17963cb12b01dda332adde964f2f
Mohamamd_Chavoshipor <mohammad.chavoshipor@gmail.com>
2021-11-22 23:28:52 +0330

    add navbar

--- src/shared/controls/SideBar.js
+++ src/shared/controls/SideBar.js
@@ -4,7 +4,10 @@ import { ReactComponent as Logo } from "shared/icons/logo.svg";
 import { ReactComponent as TV } from "shared/icons/sidebar/tv.svg";
 // planet-svgrepo-com.svg
 import { ReactComponent as PlanetSvg } from "shared/icons/sidebar/planet-svgrepo-com.svg";
-
+import { ReactComponent as Dashboard } from "shared/icons/sidebar/dashboard.svg";
+import { ReactComponent as Sell } from "shared/icons/sidebar/sell.svg";
+import { ReactComponent as Cart } from "shared/icons/sidebar/cart.svg";
+import { ReactComponent as Users } from "shared/icons/sidebar/users.svg";
 
 const SideBar = React.memo(() => {
   const history = useHistory();
@@ -29,8 +32,105 @@ const SideBar = React.memo(() => {
                     href="#"
                     className="flex gap-x-2.5 py-2.5 items-center px-6 transition duration-0.3 ease-in font-medium text-sm text-other-navLink"
                   >
-                    <TV className="icon-dashboard text-other-color" />
+                    <Dashboard className="icon-dashboard text-other-color" />
                     داشبورد
+                  </span>
+                </li>
+                <li
+                  className="cursor-pointer"
+                  onClick={() => history.push("/")}
+                >
+                  <span
+                    href="#"
+                    className="flex gap-x-2.5 py-2.5 items-center px-6 transition duration-0.3 ease-in font-medium text-sm text-other-navLink"
+                  >
+                    <Sell className="icon-dashboard text-other-color" />
+                    فروش
+                  </span>
+                </li>
+                <li
+                  className="cursor-pointer"
+                  onClick={() => history.push("/")}
+                >
+                  <span
+                    href="#"
+                    className="flex gap-x-2.5 py-2.5 items-center px-6 transition duration-0.3 ease-in font-medium text-sm text-other-navLink"
+                  >
+                    <Cart className="icon-dashboard text-other-color" />
+                    تراکنش ها
+                  </span>
+                </li>
+                {/*  */}
+                <li
+                  className="cursor-pointer"
+                  onClick={() => history.push("/")}
+                >
+                  <span
+                    href="#"
+                    className="flex gap-x-2.5 py-2.5 items-center px-6 transition duration-0.3 ease-in font-medium text-sm text-other-navLink"
+                  >
+                    <TV className="icon-dashboard text-other-color" />
+                    پروموتر ها
+                  </span>
+                </li>
+                <li
+                  className="cursor-pointer"
+                  onClick={() => history.push("/")}
+                >
+                  <span
+                    href="#"
+                    className="flex gap-x-2.5 py-2.5 items-center px-6 transition duration-0.3 ease-in font-medium text-sm text-other-navLink"
+                  >
+                    <Users className="icon-dashboard text-other-color" />
+                    لیست کاربران
+                  </span>
+                </li>
+                <li
+                  className="cursor-pointer"
+                  onClick={() => history.push("/")}
+                >
+                  <span
+                    href="#"
+                    className="flex gap-x-2.5 py-2.5 items-center px-6 transition duration-0.3 ease-in font-medium text-sm text-other-navLink"
+                  >
+                    <TV className="icon-dashboard text-other-color" />
+                    لیست شرکت ها
+                  </span>
+                </li>
+                <li
+                  className="cursor-pointer"
+                  onClick={() => history.push("/blog")}
+                >
+                  <span
+                    href="#"
+                    className="flex gap-x-2.5 py-2.5 items-center px-6 transition duration-0.3 ease-in font-medium text-sm text-other-navLink"
+                  >
+                    <TV className="icon-dashboard text-other-color" />
+                    وبلاگ
+                  </span>
+                </li>
+                <li
+                  className="cursor-pointer"
+                  onClick={() => history.push("/category")}
+                >
+                  <span
+                    href="#"
+                    className="flex gap-x-2.5 py-2.5 items-center px-6 transition duration-0.3 ease-in font-medium text-sm text-other-navLink"
+                  >
+                    <TV className="icon-dashboard text-other-color" />
+                    دسته بندی سایت
+                  </span>
+                </li>
+                <li
+                  className="cursor-pointer"
+                  onClick={() => history.push("/")}
+                >
+                  <span
+                    href="#"
+                    className="flex gap-x-2.5 py-2.5 items-center px-6 transition duration-0.3 ease-in font-medium text-sm text-other-navLink"
+                  >
+                    <TV className="icon-dashboard text-other-color" />
+                    عضویت در خبرنامه
                   </span>
                 </li>
                 <li
@@ -42,19 +142,19 @@ const SideBar = React.memo(() => {
                     className="flex gap-x-2.5 py-2.5 items-center px-6 transition duration-0.3 ease-in font-medium text-sm text-other-navLink"
                   >
                     <PlanetSvg className="icon-dashboard" fill="#fb6340" />
-                    وبلاگ
+                    سایت
                   </span>
                 </li>
                 <li
                   className="cursor-pointer"
-                  onClick={() => history.push("/category")}
+                  // onClick={() => history.push("/category")}
                 >
                   <span
                     // href="#"
                     className="flex gap-x-2.5 py-2.5 items-center px-6 transition duration-0.3 ease-in font-medium text-sm text-other-navLink"
                   >
                     <PlanetSvg className="icon-dashboard" fill="#fb6340" />
-                    دسته بندی
+                    خروج
                   </span>
                 </li>
               </ul>
