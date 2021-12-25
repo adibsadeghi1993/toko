38d9be9c76932e2cc3f63ee09597785f0a4baa0d
Mohamamd_Chavoshipor <mohammad.chavoshipor@gmail.com>
2021-12-25 15:35:11 +0330

    dynamic load newslatter

--- src/admin/newsletter/NewLetter.js
+++ src/admin/newsletter/NewLetter.js
@@ -1,8 +1,14 @@
-import React, { useContext } from "react";
+import { DEFAULT_PAGE_NUMBER, DEFAULT_ROW } from "config/constant";
+import moment from "jalali-moment";
+import React, { useContext, useEffect } from "react";
 import { NewsLetterContext } from "./state/State";
 
 export default React.memo(() => {
-  const { news } = useContext(NewsLetterContext);
+  const { subscribe } = useContext(NewsLetterContext);
+  const { getSubscribe } = useContext(NewsLetterContext);
+  useEffect(() => {
+    getSubscribe?.(DEFAULT_PAGE_NUMBER, DEFAULT_ROW);
+  }, [getSubscribe]);
   return (
     <>
       <div className="relative pb-72 h-100 z-10">
@@ -24,28 +30,34 @@ export default React.memo(() => {
             </tr>
           </thead>
           <tbody>
-            {!!news &&
-              news.map((item, index) => (
+            {!!subscribe &&
+              !!subscribe?.result?.length &&
+              subscribe?.result?.map((item, index) => (
                 <tr key={index}>
-                  <td className="text-bodyTable font-normal py-4 px-6">{item.created_At}</td>
-                  <td className="text-bodyTable font-normal py-4 pr-6">
-                    {item.active ? (
-                      <input
-                        checked="checked"
-                        className="check-box"
-                        disabled="disabl  ر  ed"
-                        type="checkbox"
-                      />
-                    ) : (
-                      <div>sal</div>
-                    )}
+                  <td className="text-bodyTable font-normal py-4 px-6" dir="ltr">
+                    {(item?.createon &&
+                      moment(item?.createon, "YYYY-MM-DD")
+                        .endOf("jMonth")
+                        .format("jYYYY/jM/jD")) ||
+                      "-"}
                   </td>
-                  <td className="text-bodyTable font-normal py-4 pr-6">{item.email}</td>
+                  <td className="text-bodyTable font-normal py-4 pr-6">
+                    <input
+                      checked="checked"
+                      className="check-box"
+                      disabled={true}
+                      type="checkbox"
+                    />
+                  </td>
+                  <td className="text-bodyTable font-normal py-4 pr-6">
+                    {item?.email}
+                  </td>
                 </tr>
               ))}
           </tbody>
         </table>
       </div>
+      ``
     </>
   );
 });
--- src/admin/newsletter/state/State.js
+++ src/admin/newsletter/state/State.js
@@ -1,45 +1,44 @@
-import React, { useReducer } from "react";
+import React, { useCallback, useContext, useReducer } from "react";
+import { SessionContext } from "shared/system-controls/session/SessionProvider";
 import NewsLetterReducer from "./Reducer";
 
 export const NewsLetterContext = React.createContext();
 
 const NewsLetterState = ({ children }) => {
-  const initialState = {
-    news: [
-      {
-        created_At: "19/08/1400 10:18:00 ق.ظ",
-        active: true,
-        email: "mahdimajidi7526@gmail.com",
-      },
-      {
-        created_At: "19/08/1400 10:18:00 ق.ظ",
-        active: true,
-        email: "mahdimajidi7526@gmail.com",
-      },
-      {
-        created_At: "19/08/1400 10:18:00 ق.ظ",
-        active: true,
-        email: "mahdimajidi7526@gmail.com",
-      },
-      {
-        created_At: "19/08/1400 10:18:00 ق.ظ",
-        active: true,
-        email: "mahdimajidi7526@gmail.com",
-      },
-      {
-        created_At: "19/08/1400 10:18:00 ق.ظ",
-        active: true,
-        email: "mahdimajidi7526@gmail.com",
-      },
-    ],
-  };
+  const initialState = {};
   const [state, dispatch] = useReducer(NewsLetterReducer, initialState);
+  const { _axios } = useContext(SessionContext);
 
+  /**
+   * @description get lists subscribes
+   * @return Array
+   * @param {number} page_number
+   * @param {number} row
+   */
+  const getSubscribe = useCallback(
+    async (page_number, row) => {
+      try {
+        let res = await _axios().get("admin_panel/subscribe", {
+          params: {
+            page_number,
+            row,
+          },
+        });
+        if (res) {
+          dispatch({ type: "SET_SUBSCRIBE", payload: res.data });
+        }
+      } catch (e) {
+        console.log("e:", e);
+      }
+    },
+    [_axios, dispatch]
+  );
   return (
     <NewsLetterContext.Provider
       value={{
         ...state,
         dispatch,
+        getSubscribe,
       }}
     >
       {children}
