bba3dc06d3033feae3e8b56874423c4c2107e213
Mohamamd_Chavoshipor <mohammad.chavoshipor@gmail.com>
2021-12-25 15:37:14 +0330

    add reducer

--- src/admin/newsletter/state/Reducer.js
+++ src/admin/newsletter/state/Reducer.js
@@ -1,9 +1,14 @@
 const reducer = (state, { type, payload }) => {
-    console.log("state: ", type, payload, state)
-    switch (type) {
-        default:
-            return state
-    }
-}
+  console.log("state: ", type, payload, state);
+  switch (type) {
+    case "SET_SUBSCRIBE":
+      return {
+        ...state,
+        subscribe: payload,
+      };
+    default:
+      return state;
+  }
+};
 
 export default reducer;
