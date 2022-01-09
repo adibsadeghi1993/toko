2139487b873632e63d0730af4f398df47a8d51d4
Mohamamd_Chavoshipor <mohammad.chavoshipor@gmail.com>
2022-01-09 09:54:07 +0330

    fix some bug

--- src/admin/members/controls/DetailsRowTableFour.js
+++ src/admin/members/controls/DetailsRowTableFour.js
@@ -44,44 +44,27 @@ export default React.memo(({ item }) => {
     <React.Fragment>
       <tr>
         <td className="py-2">
-          {/* {item?.degree_of_education_id || "-"} */}
-          <select
+        <select
             onChange={useCallback(
               (e) => {
                 dispatch({
                   type: "SET_UPDATE_DETAILS",
-                  payload: {
-                    key: "degree_of_education_id",
-                    value: e.target.value,
-                  },
+                  payload: { key: "is_married", value: e.target.value },
                 });
               },
               [dispatch]
             )}
-            className="shadow mx-auto border p-1 rounded w-full"
+            className="w-full shadow mx-auto border p-1 rounded"
           >
-            <option selected={item?.degree_of_education_id === 1} value="1">
-              زیردیپلم
+            <option value={true} selected={item?.is_married}>
+              مجرد
             </option>
-            <option selected={item?.degree_of_education_id === 2} value="2">
-              دیپلم
-            </option>
-            <option selected={item?.degree_of_education_id === 3} value="3">
-              کاردانی
-            </option>
-            <option selected={item?.degree_of_education_id === 4} value="4">
-              کارشناسی
-            </option>
-            <option selected={item?.degree_of_education_id === 5} value="5">
-              کارشناسی ارشد
-            </option>
-            <option selected={item?.know_us_id === 6} value="6">
-              دکترا
-            </option>
-            <option selected={item?.know_us_id === 7} value="7">
-              فوق دکترا
+            <option value={false} selected={!item?.is_married}>
+              متاهل
             </option>
           </select>
+          {/* {item?.degree_of_education_id || "-"} */}
+ 
         </td>
         {/* provinces */}
         <td className="py-2">
--- src/admin/members/controls/DetailsRowTableOne.js
+++ src/admin/members/controls/DetailsRowTableOne.js
@@ -71,27 +71,6 @@ export default React.memo(({ item }) => {
           />
         </td>
         <td className="py-2">
-          <select
-            onChange={useCallback(
-              (e) => {
-                dispatch({
-                  type: "SET_UPDATE_DETAILS",
-                  payload: { key: "is_married", value: e.target.value },
-                });
-              },
-              [dispatch]
-            )}
-            className="shadow mx-auto border-0 p-1 rounded"
-          >
-            <option value={true} selected={item?.is_married}>
-              مجرد
-            </option>
-            <option value={false} selected={!item?.is_married}>
-              متاهل
-            </option>
-          </select>
-        </td>
-        <td className="py-2">
           <DatePicker
             // label="تا تاریخ"
             className="shadow mx-auto border-0 p-1 rounded"
@@ -123,6 +102,45 @@ export default React.memo(({ item }) => {
               [dispatch]
             )}
           />
+        </td>
+        <td className="py-2">
+        <select
+            onChange={useCallback(
+              (e) => {
+                dispatch({
+                  type: "SET_UPDATE_DETAILS",
+                  payload: {
+                    key: "degree_of_education_id",
+                    value: e.target.value,
+                  },
+                });
+              },
+              [dispatch]
+            )}
+            className="shadow mx-auto border p-1 rounded w-full"
+          >
+            <option selected={item?.degree_of_education_id === 1} value="1">
+              زیردیپلم
+            </option>
+            <option selected={item?.degree_of_education_id === 2} value="2">
+              دیپلم
+            </option>
+            <option selected={item?.degree_of_education_id === 3} value="3">
+              کاردانی
+            </option>
+            <option selected={item?.degree_of_education_id === 4} value="4">
+              کارشناسی
+            </option>
+            <option selected={item?.degree_of_education_id === 5} value="5">
+              کارشناسی ارشد
+            </option>
+            <option selected={item?.degree_of_education_id === 6} value="6">
+              دکترا
+            </option>
+            <option selected={item?.degree_of_education_id === 7} value="7">
+              فوق دکترا
+            </option>
+          </select>
         </td>
 
       </tr>
